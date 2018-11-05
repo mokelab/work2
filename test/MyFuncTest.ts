05353798a5434b8ba0f6d36369f6250f0b7e543d
hrk fkm <fkm@mokelab.com>
2018-11-05 13:37:50 +0900

    add error case

--- test/MyFuncTest.ts
+++ test/MyFuncTest.ts
@@ -1,8 +1,11 @@
 import { describe, it } from "mocha";
 import { assert } from "chai";
 
+import { moke } from '../src/MyFunc';
+
 describe('demo', () => {
     it('case1', () => {
-        assert.equal('moke', "moke");
+        const b = moke(10);
+        assert.equal(b, 10); // bは10であるべき
     });
 })
